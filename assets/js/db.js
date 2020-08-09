85add4fe3f928fd31af217a39b034baccb43c481
imac <chadmdubay@gmail.com>
2020-08-09 02:55:58 -0400

    General update.

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,24 +1,210 @@
 var data = [
-{
-  "name":"Trainfight : ATL band @ The Caledonia 5-4-19",
-  "url":"MmBNEfsUX38"
-},
-{
-  "name":"The Buzzards of Fuzz - “I Came Back…(For My Hubcap)” Official Music Video",
-  "url":"Wsoz-hZy-jQ"
-},
+
+//Bleach Garden
 {
   "name":"Bleach Garden - Bliss (Official Video)",
-  "url":"tNZgf58Kho8"
+  "videoURL":"tNZgf58Kho8",
+  "instagramURL":"https://www.instagram.com/bleachgarden94",
+  "facebookURL":"https://www.facebook.com/BleachGarden94",
+  "bandcampURL":"https://bleachgarden94.bandcamp.com"
 },
 {
-  "name":"Rosie & The Ratdogs @ The Caledonia 9-28-18",
-  "url":"FTJWSmPG_os"
+  "name":"Bleach Garden Live at WREK",
+  "videoURL":"7cxIlFOnlV8",
+  "instagramURL":"https://www.instagram.com/bleachgarden94",
+  "facebookURL":"https://www.facebook.com/BleachGarden94",
+  "bandcampURL":"https://bleachgarden94.bandcamp.com"
 },
-{
-  "name":"Like Machines - Kaiser [Official Video]",
-  "url":"pBOUHt7H4JA"
-},
-]
 
-RA6s6_sDYbU
+//The Buzzards of Fuzz
+{
+    "name":"The Buzzards of Fuzz - I Came Back…(For My Hubcap) Official Music Video",
+    "url":"Wsoz-hZy-jQ",
+    "instagramURL":"https://www.instagram.com/thebuzzardsoffuzz",
+    "facebookURL":"https://www.facebook.com/thebuzzardsoffuzz",
+    "bandcampURL":"https://thebuzzardsoffuzz.bandcamp.com"
+},
+{
+    "name":"The Buzzards of Fuzz - How Could You? (Srisly)",
+    "url":"JRdUbY3ZJAk",
+    "instagramURL":"https://www.instagram.com/thebuzzardsoffuzz",
+    "facebookURL":"https://www.facebook.com/thebuzzardsoffuzz",
+    "bandcampURL":"https://thebuzzardsoffuzz.bandcamp.com"
+},
+
+//Death of Kings
+{
+    "name":"Death of Kings - Shadow of the Reaper",
+    "url":"kx2D2pk3P98",
+    "instagramURL":"https://www.instagram.com/deathofkings",
+    "facebookURL":"https://www.facebook.com/deathofkingsmetal",
+    "bandcampURL":"https://deathofkings.bandcamp.com/"
+},
+{
+    "name":"Death of Kings - Knifehammer",
+    "url":"IKWa1aLw8ss",
+    "instagramURL":"https://www.instagram.com/deathofkings",
+    "facebookURL":"https://www.facebook.com/deathofkingsmetal",
+    "bandcampURL":"https://deathofkings.bandcamp.com/"
+},
+
+//Deceiving Eve
+{
+    "name":"Deceiving Eve - We All Fall Apart",
+    "url":"DN2vE9C3pu0",
+    "instagramURL":"https://www.instagram.com/deceivingeveband",
+    "facebookURL":"https://www.facebook.com/deceivingeveband",
+    "bandcampURL":""
+},
+{
+    "name":"Deceiving Eve - Bitter",
+    "url":"6coA2C49bIs",
+    "instagramURL":"https://www.instagram.com/deceivingeveband",
+    "facebookURL":"https://www.facebook.com/deceivingeveband",
+    "bandcampURL":""
+}
+
+//Half Hot
+{
+    "name":"Half Hot - Nowhere (Official Video)",
+    "url":"teu1BlDk3xM",
+    "instagramURL":"https://www.instagram.com/halfhotmusic",
+    "facebookURL":"https://www.facebook.com/Halfhotmusic",
+    "bandcampURL":"https://halfhotmusic.bandcamp.com"
+},
+{
+    "name":"Half Hot - Sugar (Live at 529 EAV)",
+    "url":"5BXuDmb2sH4",
+    "instagramURL":"https://www.instagram.com/halfhotmusic",
+    "facebookURL":"https://www.facebook.com/Halfhotmusic",
+    "bandcampURL":"https://halfhotmusic.bandcamp.com"
+}
+
+//Hush Money
+{
+    "name":"Hush Money - Bourbon Street Queen (Official Video)",
+    "url":"65_uxWOTYwg",
+    "instagramURL":"https://www.instagram.com/hushmoneyband4",
+    "facebookURL":"https://www.facebook.com/HushMoneymusicinc",
+    "bandcampURL":""
+},
+{
+    "name":"Hush Money - Wickerman",
+    "url":"AYeuqQgIyN4",
+    "instagramURL":"https://www.instagram.com/halfhotmusic",
+    "facebookURL":"https://www.facebook.com/Halfhotmusic",
+    "bandcampURL":""
+}
+
+//Lazer/Wulf
+{
+    "name":"Lazer/Wulf - Choose Again",
+    "url":"HjvoJfFWwKI",
+    "instagramURL":"https://www.instagram.com/lazerwulves",
+    "facebookURL":"https://www.facebook.com/lazerwulves",
+    "bandcampURL":"https://lazerwulf.bandcamp.com"
+},
+{
+    "name":"Lazer Wulf - There Was A Hole Here, It's Gone Now (Full Album)",
+    "url":"QhXEmKmHxkc",
+    "instagramURL":"https://www.instagram.com/lazerwulves",
+    "facebookURL":"https://www.facebook.com/lazerwulves",
+    "bandcampURL":"https://lazerwulf.bandcamp.com"
+},
+
+//Like Machines
+{
+    "name":"Like Machines - Kaiser [Official Video]",
+    "url":"pBOUHt7H4JA",
+    "instagramURL":"https://www.instagram.com/likemachinesatl",
+    "facebookURL":"https://www.facebook.com/likemachinesatl",
+    "bandcampURL":""
+},
+{
+    "name":"Like Machines - Hear Freedom Ring",
+    "url":"ccgm3xL8",
+    "instagramURL":"https://www.instagram.com/likemachinesatl",
+    "facebookURL":"https://www.facebook.com/likemachinesatl",
+    "bandcampURL":""
+},
+
+//Order of the Owl
+{
+    "name":"Order of the Owl - Cocaine Super Demon",
+    "url":"4EFyL--W2ho",
+    "instagramURL":"https://www.instagram.com/orderoftheowl",
+    "facebookURL":"https://www.facebook.com/orderoftheowl",
+    "bandcampURL":"https://orderoftheowl.bandcamp.com/"
+},
+{
+    "name":"Order of the Owl - Resurrection",
+    "url":"w95v0g05xdU",
+    "instagramURL":"https://www.instagram.com/orderoftheowl",
+    "facebookURL":"https://www.facebook.com/orderoftheowl",
+    "bandcampURL":"https://orderoftheowl.bandcamp.com/"
+},
+
+//The Pinx
+{
+    "name":"The Pinx - Southern Gentleman",
+    "url":"PhYTXZDR5K0",
+    "instagramURL":"https://www.instagram.com/thepinxatl",
+    "facebookURL":"https://www.facebook.com/ThePinxATL",
+    "bandcampURL":"https://thepinx.bandcamp.com"
+},
+{
+    "name":"The Pinx - Mercy",
+    "url":"g0h6gY31WPs",
+    "instagramURL":"https://www.instagram.com/thepinxatl",
+    "facebookURL":"https://www.facebook.com/ThePinxATL",
+    "bandcampURL":"https://thepinx.bandcamp.com"
+},
+
+//Rosie & The Ratdogs
+{
+    "name":"Rosie & The Rat Dogs - Peench",
+    "url":"8HrHZIM7le8",
+    "instagramURL":"https://www.instagram.com/rosieandtheratdogs_",
+    "facebookURL":"https://www.facebook.com/rosieandtheratdogs",
+    "bandcampURL":"https://rosieandtheratdogs.bandcamp.com/"
+},
+{
+    "name":"Rosie & The Rat Dogs - Peench",
+    "url":"t8BR4KtDB_M",
+    "instagramURL":"https://www.instagram.com/rosieandtheratdogs_",
+    "facebookURL":"https://www.facebook.com/rosieandtheratdogs",
+    "bandcampURL":"https://rosieandtheratdogs.bandcamp.com/"
+},
+
+//Trainfight
+{
+    "name":"Trainfight : ATL band @ The Caledonia 5-4-19",
+    "url":"MmBNEfsUX38",
+    "instagramURL":"https://www.instagram.com/trainfight",
+    "facebookURL":"https://www.facebook.com/trainfightband",
+    "bandcampURL":"https://trainfight.bandcamp.com"
+},
+{
+    "name":"Trainfight - Why I Run (Official Music Video)",
+    "url":"aDwJGf4PmbY",
+    "instagramURL":"https://www.instagram.com/trainfight",
+    "facebookURL":"https://www.facebook.com/trainfightband",
+    "bandcampURL":"https://trainfight.bandcamp.com"
+}
+
+//Whores.
+{
+    "name":"Whores. - I See You Are Also Wearing A Black T-Shirt",
+    "url":"MgcSe12xdds",
+    "instagramURL":"https://www.instagram.com/whoresband",
+    "facebookURL":"https://www.facebook.com/whrsband",
+    "bandcampURL":"https://whores.bandcamp.com"
+},
+{
+    "name":"Whores. - Tell Me Something Scientific",
+    "url":"aPcJoRQsfQM",
+    "instagramURL":"https://www.instagram.com/whoresband",
+    "facebookURL":"https://www.facebook.com/whrsband",
+    "bandcampURL":"https://whores.bandcamp.com"
+}
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
         console.log(data);
         $("iframe").attr("src","https://www.youtube.com/embed/"+data[randomNumber].url);
         $(".videoDescription").html(data[randomNumber].name);
+        $(".followThisArtist").css("display","flex");
+        $(".linkLogosFollow").css("display","flex");
+        $("#instagramFollow").attr("href",data[randomNumber].instagramURL);
+        $("#facebookFollow").attr("href",data[randomNumber].facebookURL);
+        $("#bandcampFollow").attr("href",data[randomNumber].bandcampURL);
     })
 
 })
