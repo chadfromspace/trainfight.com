1746d9cfaa29299ae44fd019378f9940eb8e0d47
imac <chadmdubay@gmail.com>
2020-08-09 01:06:42 -0400

    Added local band button.

---
+++ assets/js/db.js
@@ -0,0 +1,24 @@
+var data = [
+{
+  "name":"Trainfight : ATL band @ The Caledonia 5-4-19",
+  "url":"MmBNEfsUX38"
+},
+{
+  "name":"The Buzzards of Fuzz - “I Came Back…(For My Hubcap)” Official Music Video",
+  "url":"Wsoz-hZy-jQ"
+},
+{
+  "name":"Bleach Garden - Bliss (Official Video)",
+  "url":"tNZgf58Kho8"
+},
+{
+  "name":"Rosie & The Ratdogs @ The Caledonia 9-28-18",
+  "url":"FTJWSmPG_os"
+},
+{
+  "name":"Like Machines - Kaiser [Official Video]",
+  "url":"pBOUHt7H4JA"
+},
+]
+
+RA6s6_sDYbU
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
-    const db = JSON.parse(db);
-
-    $(".btn").on("click",()=>{
-        console.log(db);
+    $("button").on("click",(event)=>{
+        const randomNumber = Math.floor(Math.random()*data.length);
+        console.log(data);
+        $("iframe").attr("src","https://www.youtube.com/embed/"+data[randomNumber].url);
+        $(".videoDescription").html(data[randomNumber].name);
     })
+
 })
